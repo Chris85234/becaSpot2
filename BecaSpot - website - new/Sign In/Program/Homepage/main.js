79a1b622c3e305242bb3b3f6c568d819c71992c1
Christian Alfaro <christianalfaro530@gmail.com>
2024-09-13 20:36:12 -0600

    ultimos cambios en el dap

---
+++ BecaSpot - website - new/Sign In/Program/Homepage/main.js
@@ -0,0 +1,23 @@
+
+document.getElementById('profile-image-input').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    
+    reader.onload = function(e) {
+        document.querySelector('.btn-img img').src = e.target.result;
+    };
+    
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+});
+
+document.getElementById('backButton').addEventListener('click', function() {
+    window.history.back();
+});
+
+function handleSubmit(event) {
+    event.preventDefault(); 
+    window.location.href = "Homepage/home.html"; 
+}
+
